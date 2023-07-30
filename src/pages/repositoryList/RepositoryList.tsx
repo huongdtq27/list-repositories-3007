5db3878b08c77f95b533cf9fe639a04a5c236616
Huong Milo <dtqhuong1@cmcglobal.vn>
2023-07-30 22:06:34 +0700

    list-repo: add github form user to RepositoryList, call api after update github user info

--- src/pages/repositoryList/RepositoryList.tsx
+++ src/pages/repositoryList/RepositoryList.tsx
@@ -1,19 +1,26 @@
-import React, { BaseSyntheticEvent, useMemo, useState } from "react";
-import { Alert, Input, Table } from "antd";
+import React, { BaseSyntheticEvent, useCallback, useEffect, useMemo, useState } from "react";
+import { Alert, Divider, Input, Table } from "antd";
 import { LIMIT_RECORDS, QUERY_GET_REPOSITORIES, REPOSITORY_COLUMNS, SEARCH_KEYS } from "./constants";
-import { useQuery } from "@apollo/client";
-import { DEFAULT_GIHUB_LOGIN_USER } from "../../auth";
-import "./RepositoryList.scss";
+import { useLazyQuery } from "@apollo/client";
 import { Repository } from "../../typings/repository";
+import GitHubUserForm from "../../components/gitHubUserForm";
+import { DEFAULT_GIHUB_LOGIN_USER } from "../../auth";
+
+import "./RepositoryList.scss";
 
 const { Search } = Input;
 
 export const RepositoryList: React.FC = () => {
   const [searchValue, setSearchValue] = useState("");
 
-  const { loading, error, data } = useQuery(QUERY_GET_REPOSITORIES, {
-    variables: { limit: LIMIT_RECORDS, loginUser: DEFAULT_GIHUB_LOGIN_USER },
+  const gitHubLoginUser = localStorage.getItem("gitHubUser");
+  const [loadRepositories, { called, loading, error, data }] = useLazyQuery(QUERY_GET_REPOSITORIES, {
+    variables: { limit: LIMIT_RECORDS, loginUser: gitHubLoginUser || DEFAULT_GIHUB_LOGIN_USER },
   });
+
+  useEffect(() => {
+    loadRepositories();
+  }, [loadRepositories]);
 
   const originalRepositories = useMemo(() => {
     if (loading || error) {
@@ -34,23 +41,37 @@ export const RepositoryList: React.FC = () => {
         record[recordKey as keyof Repository]?.toString().toLowerCase().includes(trimLowerCaseSearchValue)
       )
     );
-    return filteredList as Repository[];
-  }, [originalRepositories, searchValue]);
+    return filteredList;
+  }, [searchValue, originalRepositories]);
 
   const onChangeSearch = ({ target: { value } }: BaseSyntheticEvent) => {
     setSearchValue(value);
   };
+  const onFinishGitHubForm = useCallback(() => {
+    loadRepositories();
+  }, [loadRepositories]);
 
   return (
     <div className="list-repository-page">
       {error && <Alert message={error.message} type="error" showIcon closeIcon />}
 
       <h2>GITHUB REPOSITORIES</h2>
+
+      {/* GitHub User Info */}
+      <Divider orientation="left">
+        <h3>User Infomation</h3>
+      </Divider>
+      <GitHubUserForm onFinishForm={onFinishGitHubForm} />
+
+      {/* Table */}
+      <Divider orientation="left">
+        <h3>List Repository</h3>
+      </Divider>
       <Search onChange={onChangeSearch} className="search-field" size="large" placeholder="Search repository" />
       <Table
         columns={REPOSITORY_COLUMNS}
         dataSource={filteredRepositories}
-        loading={loading}
+        loading={called && loading}
         className="list-repository-table"
       />
     </div>
